5a465fe44bf8e72878c7ef7cbb1c25c522adb100
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-10 18:16:39 -0500

    Modified 'act3-q1.rb' by modifying the 'loop' code to be a double loop, containing an '.each' outer loop block for dealing with each 'row' of data in 'example.csv', and an inner iterator loop block for creating the necessary hashes that will contain the aforementioned .csv file's data.

--- act3-q1.rb
+++ act3-q1.rb
@@ -1,8 +1,42 @@
 
+# This program reads the contents of a .csv file, and arranges them
+# into a Ruby array, and then into a Ruby hash.
 
-new_list = []
+header_array = []
+data_array = []
+hash_array = []
+is_header = 0
 
 File.foreach("example.csv") do |line|
-    new_list << line
+
+    if is_header == 0
+        header_array << line.split(',')
+        is_header = 1
+
+    else
+        data_array << line.split(',')
+    end
 
 end
+
+# The three lines below are unused test code.
+#puts header_array
+#puts data_array
+#puts data_array[0]
+
+row_no = 0
+
+data_array.each do |row|
+
+    hash_array << {}
+
+    (0..2).each do |x|
+        puts header_array[x]
+        puts row[x]
+    end
+
+    row_no += 1
+
+end
+
+puts hash_array
